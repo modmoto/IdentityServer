bc29ef74a2d25e18d3e74822e1627a41d8a8cf62
modmoto <simonheiss87@gmail.com>
2021-08-27 21:44:01 +0200

    ll

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -29,8 +29,8 @@ namespace IdentityServer
 
                     AllowedGrantTypes = GrantTypes.Code,
             
-                    RedirectUris = { $"https://{Environment.GetEnvironmentVariable("IDENTITY_BASE_URI")}/signin-oidc" },
-                    PostLogoutRedirectUris = { $"https://{Environment.GetEnvironmentVariable("IDENTITY_BASE_URI")}/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:5000/signin-oidc", $"https://{Environment.GetEnvironmentVariable("IDENTITY_BASE_URI")}/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:5000/signout-callback-oid", $"https://{Environment.GetEnvironmentVariable("IDENTITY_BASE_URI")}/signout-callback-oidc" },
 
                     AllowedScopes = new List<string>
                     {
