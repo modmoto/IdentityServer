ac120f4d7dc1f16ad0433fa86bb16e43ae2488ba
modmoto <simonheiss87@gmail.com>
2021-09-02 18:21:57 +0200

    remember login is 30

--- IdentityServer/Quickstart/Account/AccountOptions.cs
+++ IdentityServer/Quickstart/Account/AccountOptions.cs
@@ -6,7 +6,7 @@ namespace IdentityServer.Quickstart.Account
     {
         public static bool AllowLocalLogin = true;
         public static bool AllowRememberLogin = true;
-        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromMinutes(1);
+        public static TimeSpan RememberMeLoginDuration = TimeSpan.FromDays(30);
 
         public static bool ShowLogoutPrompt = true;
         public static bool AutomaticRedirectAfterSignOut = false;
